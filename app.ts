0a282b34ba12d25a1eb921ebb7fd11384fe8eace
n-a-n <loic.truchot35@gmail.com>
2018-02-18 14:53:19 +0100

    feat(proxy): proxy test

--- app.ts
+++ app.ts
@@ -1,3 +1,24 @@
+class Item {
+  is: string[];
+  constructor(public word: string) {
+    this.is = [];
+  }
+}
+const wordEntitites: { [s: string]: Item } = {
+  is: new Item("is")
+};
+const words = new Proxy(wordEntitites, {
+  set(o, p, v) {
+    // console.log("set", p, o, v);
+    o[p] = v;
+    return true;
+  },
+  get(o, p) {
+    // console.log("get", p, o);
+    return o[p] && o[p].word;
+  }
+});
+
 const iptText: HTMLInputElement = document.getElementById(
   "iptText"
 ) as HTMLInputElement;
@@ -5,8 +26,11 @@ const btnValidate: HTMLButtonElement = document.getElementById(
   "btnValidate"
 ) as HTMLButtonElement;
 btnValidate.addEventListener("click", () => {
-  const words = iptText.value.split(" ");
-  words.forEach((word: string) => {
-    console.log(word);
+  const items = iptText.value.split(" ");
+  items.forEach((item: string) => {
+    if (!words[item]) {
+      words[item] = new Item(item);
+    }
   });
+  console.log(words["sky"], wordEntitites["sky"].is);
 });
