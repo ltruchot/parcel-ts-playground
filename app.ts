1d5322a3ae4d08e296fcc0e007fb4eec8f0c50a4
n-a-n <loic.truchot35@gmail.com>
2018-02-18 11:07:18 +0100

    ready to work environment

--- app.ts
+++ app.ts
@@ -1,1 +1,12 @@
-console.log('test');
+const iptText: HTMLInputElement = document.getElementById(
+  "iptText"
+) as HTMLInputElement;
+const btnValidate: HTMLButtonElement = document.getElementById(
+  "btnValidate"
+) as HTMLButtonElement;
+btnValidate.addEventListener("click", () => {
+  const words = iptText.value.split(" ");
+  words.forEach((word: string) => {
+    console.log(word);
+  });
+});
